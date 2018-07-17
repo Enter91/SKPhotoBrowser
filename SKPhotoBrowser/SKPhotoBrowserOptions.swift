124179c7885224a94ca3e2b383220621b04be1ff
Michał Czwarnowski <mczwarnowski@interia.pl>
2018-07-17 09:07:40 +0200

    Update SKPhotoBrowserOptions.swift

--- SKPhotoBrowser/SKPhotoBrowserOptions.swift
+++ SKPhotoBrowser/SKPhotoBrowserOptions.swift
@@ -63,4 +63,5 @@ public struct SKToolbarOptions {
     public static var textColor: UIColor = .white
     public static var font: UIFont = .systemFont(ofSize: 17.0)
     public static var textShadowColor: UIColor = .black
+    public static var displayToolbarAtBottom: Bool = true
 }
